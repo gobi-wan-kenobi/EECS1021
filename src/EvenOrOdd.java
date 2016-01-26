07ee40f8cc7a0a263bffeebaebcbf87738eaec6d
Gobi Nadesh <gobi.nadesh@gmail.com>
2016-01-26 00:06:57 -0500

    Commit all the files

---
+++ src/EvenOrOdd.java
@@ -0,0 +1,15 @@
+public class EvenOrOdd
+{
+	public static void main(String[] args)
+	{
+		int numberToTest = 7;
+		if (numberToTest % 2 == 0)
+		{
+			System.out.printf("The number %d is even.\n", numberToTest);
+		}
+		else
+		{
+			System.out.printf("The number %d is odd.\n", numberToTest);
+		}
+	}
+}
